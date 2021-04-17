b678186c012e9e80f1a52f2ce7ea54596d6ffb26
marijntepas <marijntepas@gmail.com>
2021-04-17 20:08:16 +0200

    Added ASMHooks

---
+++ src/main/java/vazkii/quark/base/asm/ASMHooks.java
@@ -0,0 +1,49 @@
+package vazkii.quark.base.asm;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.BlockPistonStructureHelper;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.inventory.GuiBeacon;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntityPiston;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import vazkii.quark.vanity.client.emotes.EmoteHandler;
+
+import javax.annotation.Nullable;
+
+@SuppressWarnings("unused")
+public final class ASMHooks {
+
+	// ===== EMOTES ===== //
+
+	public static void updateEmotes(Entity e) {
+		EmoteHandler.updateEmotes(e);
+	}
+}
